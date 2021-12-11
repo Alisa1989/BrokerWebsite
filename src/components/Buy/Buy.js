87f34230e71035685281f5afab6048098d217321
Codelleschi <a.steinhauslin@gmail.com>
2021-12-10 20:58:47 -0800

    started styling navbar

--- src/components/Buy/Buy.js
+++ src/components/Buy/Buy.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Buy.css'
 
 const Buy = () => {
     return(
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,64 @@
 import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import {Navbar as Nav, NavbarBrand, NavbarToggler, NavItem, NavLink, UncontrolledDropdown, DropdownItem, DropdownToggle, Collapse, DropdownMenu, NavbarText} from "reactstrap";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Buy">Buy</Link>
-        </li>
-        <li>
-          <Link to="/Sell">Sell</Link>
-        </li>
-    </div>
+  <Nav
+    color="primary"
+    dark
+    expand="md"
+    fixed="top"
+  >
+    <NavbarBrand href="/">
+      Jon Smith Broker
+    </NavbarBrand>
+    <NavbarToggler onClick={function noRefCheck(){}} />
+    <Collapse navbar>
+      <Nav
+        className="me-auto"
+        navbar
+      >
+        <NavItem>
+          <Link to="/">
+            Home
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/Buy">
+            Buy
+          </Link>
+        </NavItem>
+        <UncontrolledDropdown
+          inNavbar
+          nav
+        >
+          <DropdownToggle
+            caret
+            nav
+          >
+            Geography
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>
+              Option 1
+            </DropdownItem>
+            <DropdownItem>
+              Option 2
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>
+              Reset
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+      <NavbarText>
+        Simple Text
+      </NavbarText>
+    </Collapse>
+  </Nav>
   );
 };
 
