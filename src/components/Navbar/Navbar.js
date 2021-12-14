d594d05627b816f37888987869597b0ed9315465
Codelleschi <a.steinhauslin@gmail.com>
2021-12-13 19:57:13 -0800

    built navbar ground up

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,65 +1,47 @@
 import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import {Navbar as Nav, NavbarBrand, NavbarToggler, NavItem, NavLink, UncontrolledDropdown, DropdownItem, DropdownToggle, Collapse, DropdownMenu, NavbarText} from "reactstrap";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [geoDropdown, setGeoDropdown] = useState(false);
+
+  const toggleGeoDropdown = () => setGeoDropdown(geoDropdown);
+
   return (
-  <Nav
-    id="navigation"
-    color="primary"
-    dark
-    expand="md"
-    fixed="top"
-  >
-    <NavbarBrand href="/">
-      Jon Smith Broker
-    </NavbarBrand>
-    <NavbarToggler onClick={function noRefCheck(){}} />
-    <Collapse navbar>
-      <Nav
-        className="me-auto"
-        navbar
-      >
-        <NavItem>
-          <Link to="/" >
-            Home
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/Buy">
-            Buy
-          </Link>
-        </NavItem>
-        <UncontrolledDropdown
-          inNavbar
-          nav
-        >
-          <DropdownToggle
-            caret
-            nav
-          >
-            Geography
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>
-              Sacramento County
-            </DropdownItem>
-            <DropdownItem>
-              Placer County
-            </DropdownItem>
-            <DropdownItem>
-              El Dorado County
-            </DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </Nav>
-      <NavbarText>
-        Phone: &nbsp; 
-      </NavbarText>
-      <a className="call-link" href="tel:5890000111">589-0000111</a>
-    </Collapse>
-  </Nav>
+    <div className="navbar-container">
+        <div className="navbar-navigation">
+        <h3>Jon Carrot</h3>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Buy">Buy</Link>
+        </li>
+        <li>
+          <Link to="/Sell">Sell</Link>
+        </li>
+        <div className="dropdown">
+          <button className="button-geo">Geography</button>
+          <div className="dropdown-content">
+          <a href="/Sell">Placer County</a>
+          <a href="/Sell">El Dorado County</a>
+          <a href="/Sell">Sacramento County</a>
+        </div>
+        </div>
+        </div> 
+      <div>
+
+      <span>Call Us: &nbsp;</span>
+      <a className="contact-link" href="tel:5890000111">
+        589-0000111
+      </a>
+      <span>&nbsp; &nbsp; &nbsp; E-mail:  &nbsp;</span>
+      <a className="contact-link" href="mailto: abc@example.com">
+        abc@example.com
+      </a>
+      </div>
+    </div>
   );
 };
 
