ae6cacc25c808d58d64bf0610a1b764eee637ee9
Codelleschi <a.steinhauslin@gmail.com>
2021-12-16 20:53:25 -0800

    first draft of responsive navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import smallImage from "../../images/jhesse.jpg";
+import {faPhone, faEnvelope, faBars} from "@fortawesome/free-solid-svg-icons";
+import {faTwitter, faPinterest, faFacebook, faFontAwesome} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
 const Navbar = () => {
   const [geoDropdown, setGeoDropdown] = useState(false);
@@ -9,9 +13,27 @@ const Navbar = () => {
   const toggleGeoDropdown = () => setGeoDropdown(geoDropdown);
 
   return (
-    <div className="navbar-container-big-screens">
-      {/* <img className="navbar-image" src={logoImage} alt="logo"/> */}
+    <div className="navbar-container">
     <div className="navbar-menu">
+      <div className="contact-links-mobile">
+      <a className="contact-link" href="tel:9165420274">
+        <FontAwesomeIcon className="contact-icon" icon={faPhone}/>
+      </a>
+      <a className="contact-link" href="mailto: jon@acceleratedrg.com">
+      <FontAwesomeIcon className="contact-icon" icon={faEnvelope}/>
+      </a>
+      </div>
+      <img className="navbar-image" src={smallImage} alt="small-logo" />
+      <div className="mobile-dropdown">
+          <button className="mobile-menu">
+            <FontAwesomeIcon icon={faBars}/>
+          </button>
+          <div className="dropdown-content-mobile">
+          <a href="/Home">Home</a>
+          <a href="/Buy">Buy</a>
+          <a href="/Sell">Sell</a>
+        </div>
+        </div>
         <div className="navbar-navigation">
         <li>
           <Link to="/">Home</Link>
@@ -34,12 +56,12 @@ const Navbar = () => {
       <div className="navbar-links">
 
       <span>Phone: &nbsp;</span>
-      <a className="contact-link" href="tel:5890000111">
-        589-0000111
+      <a className="contact-link" href="tel:9165420274">
+        (916)542-0274
       </a>
       <span>&nbsp; &nbsp; E-mail:  &nbsp;</span>
-      <a className="contact-link" href="mailto: abc@example.com">
-        abc@example.com
+      <a className="contact-link" href="mailto: jon@acceleratedrg.com">
+      jon@acceleratedrg.com
       </a>
       </div>
     </div>
