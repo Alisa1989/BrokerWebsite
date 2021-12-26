d7e6127f7216301d8eefa03ec5db140ec536b16c
Codelleschi <a.steinhauslin@gmail.com>
2021-12-26 10:01:01 -0800

    footer added and styled

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './Footer.css';
+import {faTwitter, faPinterest, faFacebookSquare, faFontAwesome, faTeamspeak} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
     return(
-        <footer>Contact</footer>
+        <footer>
+            <div className="footer-content">
+            <p>
+            4120 Douglas Blvd. #306-275 <br/>
+Granite Bay, CA 95746
+            </p>
+            <div className='footer-icons'>
+                <a href="https://www.facebook.com/JonHesseBroker">
+                <FontAwesomeIcon icon={faFacebookSquare} color="white"/>
+                </a>
+                <a href="http://www.pinterest.com/jonhessebroker/">
+                <FontAwesomeIcon icon={faPinterest} color="white"/>
+                </a>
+            </div>
+            </div>
+        </footer>
     )
 }
 
