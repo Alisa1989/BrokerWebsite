86b0d983c4ad4ca7e29cf9790d3df572a189b720
Codelleschi <a.steinhauslin@gmail.com>
2021-12-28 02:30:00 -0800

    added content for sell page and styled it

--- src/components/Sell/Sell.js
+++ src/components/Sell/Sell.js
@@ -1,9 +1,181 @@
-import React from 'react';
+import React from "react";
+import "./Sell.css";
+import logoImage from "../../images/logo1.jpg";
 
 const Sell = () => {
-    return(
-        <p>Sell</p>
-    )
-}
+  return (
+    <div className="sell-container">
+      <h1>Selling</h1>
+      <p>
+        &emsp; Selling a home is just as complicated a process as buying one,
+        and as such, it’s in your best interest to work with a skilled
+        Sacramento real estate agent who can ensure that all bases are covered
+        from beginning to end.
+      </p>
+      <p>
+        &emsp; Jon Hesse has a decade of experience assisting residents of
+        Sacramento’s three main counties — El Dorado, Placer and Sacramento —
+        who are looking to sell their properties and relocate elsewhere. He can
+        help you with the following steps that are key to the best home-selling
+        experience:
+      </p>
+      <ol>
+        <li className="sell-list-numbered">
+          <b> Setting a listing price for your home </b> - As a seasoned
+          Sacramento real estate agent, Jon knows the ins and outs of the
+          Sacramento housing market. Armed with this information, he’ll assess
+          your property and come up with the right listing price based on
+          similar houses that have recently sold in your area.
+        </li>
+        <li className="sell-list-numbered">
+          <b> Staging your home </b> - Before you place your residence on the
+          market, you’ll probably need to do a little bit of work to get it in
+          proper viewing condition. Jon can help you prioritize these tasks and
+          ensure that your property looks appealing to prospective buyers.
+        </li>
+        <li className="sell-list-numbered">
+          <b> Advertising and showing your home</b> - One of the main advantages
+          of working with a Sacramento real estate agent is that they know how
+          to market your house and get it in front of the right people. Jon will
+          make sure that your listing perfectly highlights your home’s selling
+          points so that it attracts more buyers.
+        </li>
+        <li className="sell-list-numbered">
+          <b>Negotiating a deal </b>- Once you start receiving offers for your
+          residence, Jon will review the details with you and provide helpful
+          advice to influence your decision. Then, he’ll guarantee that all
+          transactions are completed by the closing date.
+        </li>
+      </ol>
+      <p>
+        &emsp; You can count on Jon to adhere to the highest standards of
+        professionalism throughout the entire home-selling process. He’s
+        committed to each and every one of his clients and isn’t happy unless
+        they are.
+      </p>
+      <p>&emsp; Call Jon today to schedule an appointment.</p>
+      <h2>What You Need To Know</h2>
+      <p>
+        Selling a home can be a complex process, but there are steps you can
+        take to ensure that your property is in high demand. Here are some key
+        home-selling tips to keep in mind:
+      </p>
+      <ul>
+        <li className="sell-list-unordered">
+          <b>Find yourself a real estate professional </b> – Jon Hesse has
+          plenty of experience selling homes, and he knows what it takes to
+          attract the right kind of buyers. Use Jon Hesse and his expert team of
+          professionals as a resource along the way.
+        </li>
+        <li className="sell-list-unordered">
+          <b>Boost curb appeal - </b>First impressions can be the most important
+          part, so spend some time improving your yard, driveway and the
+          exterior of your home. You want the buyers to “get out of their car.”
+          This should make your prospective buyers feel good about your house
+          before they’ve even walked inside.
+        </li>
+        <li className="sell-list-unordered">
+          <b>Fix up your home </b>- The majority of buyers like properties that
+          are in move-in condition, meaning they won’t have to make many
+          repairs, if any at all. With this in mind, it could benefit you in the
+          long run to address any issues before putting your house on the
+          market. Jon can shed some great light on this subject all the while
+          saving you money.
+        </li>
+        <li className="sell-list-unordered">
+          <b>Highlight selling points </b>- Buyers tend to want things like
+          storage space and natural light, so work on highlighting these key
+          selling points. Jon can help you identify the key features of your
+          home and the best way to showcase them.
+        </li>
+        <li className="sell-list-unordered">
+          <b>Set the right price </b>– Understandably, you’ll want to make as
+          much money as possible selling your home. Jon will show you all the
+          necessary data and local market trends when assisting you with setting
+          the proper price. The last thing he wants is for your home to sit
+          stagnant on the market without any offers.
+        </li>
+        <li className="sell-list-unordered">
+          <b>Stage the interior of your home </b>- Most prospective buyers like
+          to envision themselves living in your home, which includes placing
+          their furniture and belongings inside it. Sometimes they can have a
+          hard time doing this if your personal belongings, knickknacks,
+          photographs and unique decor are on display. It may be advantageous to
+          seek the professional advice of a “stager” to give your home a
+          welcoming neutral feeling for all.
+        </li>
+        <li className="sell-list-unordered">
+          <b>Sweeten the deal </b>- By offering to put money toward closing
+          costs or remaining flexible about dates and deadlines, you may be able
+          to sell your property faster. Jon will share his tips of the trade
+          when the time is right.
+        </li>
+      </ul>
+      <p>
+        &emsp; If you live in the Greater Sacramento and surrounding areas and
+        are thinking about putting your Placer County, Sacramento County or El
+        Dorado County real estate on the market, Jon Hesse is your #1 source and
+        is here to assist you from start to closing. Call Jon today to see how
+        he can help you with all of your real estate needs.
+      </p>
+      <h2>Short Sales</h2>
+      <p>
+      &emsp; Sometimes we as homeowners in the Greater Sacramento area experience
+        major financial setbacks that cause us to fall behind on our mortgage
+        payments, which can ultimately lead to a foreclosure of our property.
+        That being said, there are alternatives to foreclosure that we should
+        look into and try to take advantage of with the help of an experienced
+        Sacramento real estate agent. One of these alternatives is known as a
+        short sale.
+      </p>
+      <p>&emsp; According to Wikipedia, a short sale can be defined as follows:</p>
+      <p>
+      &emsp; “A short sale is a sale of real estate in which the proceeds from
+        selling the property will fall short of the balance of debts secured by
+        liens against the property, and the property owner cannot afford to
+        repay the liens’ full amounts and where the lien holders agree to
+        release their lien on the real estate and accept less than the amount
+        owed on the debt. Any unpaid balance owed to the creditors is known as a
+        deficiency. Short sale agreements do not necessarily release borrowers
+        from their obligations to repay any shortfalls on the loans, unless
+        specifically agreed to between the parties. However, in California,
+        legislation was passed to preclude deficiencies after a short sale is
+        approved. The same is true of lenders on first loans and lenders on
+        second loans — once the short sale is approved, no deficiencies are
+        permitted after the short sale. (SB 931, SB 458 – Calif. Code of Civil
+        Procedure §580e).”
+      </p>
+      <p>
+      &emsp; A short sale is considered preferable to a foreclosure for some of the
+        following reasons:
+      </p>
+
+      <ul>
+        <li className="sell-list-unordered">It is less damaging to the homeowner’s credit</li>
+        <li className="sell-list-unordered">It is mutually beneficial to the homeowner and the lender</li>
+        <li className="sell-list-unordered">
+          It relieves the homeowner of the debts secured against the property
+        </li>
+        <li className="sell-list-unordered">
+          It should elevate a possible eviction from a foreclosure or trustee
+          sale.
+        </li>
+      </ul>
+      <p>
+        If you’re interested in finding out how you can sell your home through a
+        short sale — or, on the other hand, if you would like to learn more
+        about purchasing a short sale — it’s important to find a professional in
+        the Greater Sacramento area who specializes in these complex
+        transactions. Jon Hesse, one of the top real estate brokers in Northern
+        California, has years of experience assisting clients through the short
+        sale process. Jon and his team are prepared to do everything it takes to
+        help you buy or sell a property, giving you the personal care and
+        attention you deserve.
+      </p>
+      <p>&emsp; Make the smart choice and call Jon today to get started!</p>
+      <img className="buy-image" src={logoImage} alt="logo" />
+    </div>
+  );
+};
 
 export default Sell;
