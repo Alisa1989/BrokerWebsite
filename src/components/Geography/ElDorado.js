2b179d6170eee5f2895042ee13fc9d94be371502
Codelleschi <a.steinhauslin@gmail.com>
2021-12-31 09:13:37 -0800

    added and styled regions content

--- src/components/Geography/ElDorado.js
+++ src/components/Geography/ElDorado.js
@@ -1,11 +1,55 @@
 import React from "react";
 
 const ElDorado = () => {
-    return(
-        <div>
-            <h1>El Dorado County</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>El Dorado County</h1>
+      <p>
+        &emsp; El Dorado County in California is an area known for its robust
+        natural beauty and close proximity to some of the hottest destinations
+        in the Golden State. As it’s a relatively short drive from Sacramento,
+        it has become an increasingly popular spot for families, young adults
+        and retirees who enjoy being close to a large metro area, yet immersed
+        in gorgeous surroundings.
+      </p>
+      <p>
+        &emsp; Consisting of 1,805 square miles of gorgeous hills and
+        mountainous terrain, the El Dorado County website describes the region’s
+        geography as follows:
+      </p>
+      <p>
+        &emsp; “The County’s western boundary contains part of Folsom Lake, and
+        the eastern boundary is also the California-Nevada State line. The
+        County is topographically divided into two zones. The northeast corner
+        of the County is in the Lake Tahoe basin, while the remainder of the
+        County is in the ‘western slope,’ the area west of Echo Summit. This
+        landscape invites residents and tourists alike to enjoy outdoor
+        recreation activities year round.”
+      </p>
+      <p>
+        &emsp; The most populous city in El Dorado County is South Lake Tahoe,
+        which sits right on the border of Nevada. South Lake Tahoe is home to
+        Heavenly Mountain Resort, a popular destination for skiers and
+        snowboarders from all over the country. The temperatures are relatively
+        mild year round, reaching an average of 80 degrees Fahrenheit in the
+        summer and an average of 43 degrees Fahrenheit in the winter.
+      </p>
+      <p>
+        &emsp; Individuals interested in purchasing or selling a property in El
+        Dorado County can count on Jon Hesse, one of the top real estate brokers
+        in the greater Sacramento area, to assist them at their convenience. Jon
+        has years of experience guiding people through complex buying and
+        selling processes and ensuring that they are satisfied from beginning to
+        end. Throughout his impressive career, he has conducted more than $100
+        million worth of real estate transactions, giving him an abundance of
+        knowledge and experience.
+      </p>
+      <p>
+        &emsp; To find out more about El Dorado County real estate for sale or
+        place your house on the market, call Jon today!
+      </p>
+    </div>
+  );
+};
 
 export default ElDorado;
--- src/components/Geography/Sacramento.js
+++ src/components/Geography/Sacramento.js
@@ -1,11 +1,53 @@
 import React from "react";
 
 const Sacramento = () => {
-    return(
-        <div>
-            <h1>Sacramento County</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Sacramento County</h1>
+      <p>
+        &emsp; If you’re thinking about relocating in California and you’re
+        looking for a metro area with affordable housing and easy access to all
+        sorts of amenities and attractions, you should consider viewing some of
+        the excellent homes for sale in Sacramento County and the surrounding
+        area.
+      </p>
+      <p>
+        &emsp; Sacramento County is the most populous of the eight counties in
+        the Greater Sacramento area. It covers about 994 square miles and is
+        home to approximately 1.4 million people. It includes the low delta
+        lands located between the Sacramento River and San Joaquin River as well
+        as the foothills of the Sierra Nevada Mountains. It even provides direct
+        access to the Bay Area at certain points, making it a cost-effective
+        alternative to the expensive San Francisco lifestyle. You can get a much
+        more spacious and affordable property in Sacramento County than you can
+        in the Bay Area.
+      </p>
+      <p>
+        &emsp; The biggest city in Sacramento County is the state capital of
+        Sacramento itself, which is the sixth-largest city in the Golden State.
+        There’s a lot to love about Sacramento, which TIME once named “America’s
+        Most Diverse City.” One of the best things about Sacramento is how
+        naturally beautiful it is. It’s known as the City of Trees for its
+        abundance of plants.
+      </p>
+      <p>
+        &emsp; Some of the other cities and towns in Sacramento County include
+        Folsom, Orangevale, Citrus Heights, Rancho Cordova, Elk Grove, Galt and
+        Isleton, to name a few. Each part of the county has its own unique vibe,
+        which you can come to better understand with the help of Jon Hesse, one
+        of the area’s most successful real estate brokers.
+      </p>
+      <p>
+        &emsp; Jon has years of experience assisting buyers and sellers in the
+        Greater Sacramento area and has conducted more than $100 million worth
+        of real estate transactions. He has a deep understanding of the buying
+        and selling processes, and he and his team strive to ensure that each
+        and every client is 100 percent satisfied. If you’re hoping to buy or
+        sell a property in Sacramento County, Jon is here to give you his
+        undivided attention and assistance from beginning to end.
+      </p>
+    </div>
+  );
+};
 
 export default Sacramento;
